08035af10a5491ab85043195d2231b65b34257ba
jlrusso <jlrusso24@gmail.com>
2020-09-27 14:47:35 -0700

    Add Typescript Support

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,12 +1,13 @@
-<script>
-  import Layout from '../Shared/Layout'
+<script lang='ts'>
+  import Layout from '../Shared/Layout.vue';
+  import Vue from 'vue'
 
-  export default {
+  export default Vue.extend({
     name: 'Home',
     components: {
       Layout
     }
-  }
+  })
 </script>
 
 <template>
